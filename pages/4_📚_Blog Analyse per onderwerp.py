588766559c6b6d6707df1787685d2972620f2457
Jesse van Elteren <jvanelteren@gmail.com>
2023-09-23 14:24:17 +0200

    updated link to blog

--- pages/4_📚_Blog Analyse per onderwerp.py
+++ pages/4_📚_Blog Analyse per onderwerp.py
@@ -4,5 +4,5 @@ import streamlit as st
 
 st.set_page_config(page_title="Analyse per onderwerp", page_icon="📚")
 
-src = 'https://jvanelteren.github.io/blog/posts/energy/2023-05-22-prices.html'
+src = 'https://jvanelteren.github.io/blog/posts/moties2023/2023-09-11-kamermoties_topics.html'
 st.components.v1.iframe(src, width=800, height=5500, scrolling=False)
