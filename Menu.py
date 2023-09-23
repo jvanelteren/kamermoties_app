e6ce637295d6b8eb7247665bdb0e09700d51d514
Jesse van Elteren <jvanelteren@gmail.com>
2023-09-23 17:06:07 +0200

    changed menu

--- Menu.py
+++ Menu.py
@@ -5,25 +5,31 @@ st.set_page_config(
     page_icon="👋",
 )
 
-st.write("# Welkom bij de MotieMachine! 👋")
+st.write("# Welkom bij de MotieMachine")
 
 
 
 st.markdown(
     """
-    De afgelopen kamerperiode zijn er meer dan 9000 moties behandeld. Ik heb hier een 🔎tool voor gemaakt en enkele 📚 blogs over geschreven. Ook komt er nog een 🧠 ChatGPT analyse bij.
+    De afgelopen kamerperiode zijn er meer dan 9000 moties behandeld. Ik heb hier een tool voor gemaakt en enkele blogs over geschreven. Ook komt er nog een ChatGPT analyse bij.
     
-    ** 👈 Kies linksboven om de tool te starten **
-    
-    
+    👈 **Kies uit het menu linksboven om te starten**
     
     
-    ### Dank gaat uit naar
+    
+    
+    
+    
+    
+    
+
+    
+    Heb je een inzichten opgedaan, feedback op de app of wil je contact opnemen? Doe dit bijvoorbeeld via [LinkedIn](https://www.linkedin.com/in/jessevanelteren/).
+    
+    Dank gaat uit naar
 
     * :sun_with_face: [Tweede Kamer Open Data Portaal](https://opendata.tweedekamer.nl/) voor het beschikbaar maken van de moties
     * :sun_with_face: Dimo Angelov, bedenker en ontwikkelaar van [Top2Vec](https://github.com/ddangelov/Top2Vec)
-    
-    Heb je een inzichten opgedaan, feedback op de app of wil je contact opnemen? Doe dit bijvoorbeeld via [LinkedIn](https://www.linkedin.com/in/jessevanelteren/).
 
     [![License: Creative Commons Naamsvermelding-GelijkDelen 4.0 Internationaal-licentie](https://i.creativecommons.org/l/by-sa/4.0/80x15.png)](https://creativecommons.org/licenses/by-sa/3.0/) 2023 Jesse van Elteren
     """
